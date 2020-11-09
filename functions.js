db2e662d303f4eec044079722ef141b2e7bdf062
Hans Kool <Hansjekool@hotmail.com>
2020-11-09 14:20:25 +0100

    mod6 jest basics

--- functions.js
+++ functions.js
@@ -1,5 +1,20 @@
+const axios = require(`axios`);
+// window.axios = require("axios");
+
 const functions = {
-    add: (num1, num2) => num1 + num2
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstname: "Hans" };
+    user["lastname"] = "Kool";
+    return user;
+  },
+  fetchUser: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.data)
+      .catch((err) => `error ${err}`),
 };
 
 module.exports = functions;
