31d9b2d053dc75fc7d86d53427ee5f6d792d7f7b
Hans Kool <Hansjekool@hotmail.com>
2020-11-12 09:13:37 +0100

    begin omgevingsfactoren

--- groentetuin.js
+++ groentetuin.js
@@ -1,24 +1,6 @@
-const corn = {
-  name: "corn",
-  yield: 30,
-  factors: {
-    sun: {
-      low: -50,
-      medium: 0,
-      high: 50,
-    },
-  },
-};
+const yieldMultiplier = 0.5;
 
-const environment_factors = {
-  sun: "low",
-};
-
-// const isNotNull = (str) => str !== null;
-
-const yieldMultiplier = 1;
-
-const get_yield_for_plant = (crop) => {
+const get_yield_for_plant = (crop, environment_factors) => {
   return crop.yield * yieldMultiplier;
 };
 
@@ -32,12 +14,30 @@ const get_total_yield = ({ crops }) => {
   return cropTotal.reduce((a, b) => a + b, 0);
 };
 
-const get_profit_for_crop = () => 100;
+const get_profit_for_crop = (input) => {
+  return get_revenue_for_crop(input) - get_costs_for_crop(input);
+};
 
-const get_total_profit = () => 1000;
+const get_total_profit = ({ crops }) => {
+  const cropTotal = crops.map((item) => {
+    return get_revenue_for_crop(item) - get_costs_for_crop(item);
+  });
+  return cropTotal.reduce((a, b) => a + b, 0);
+};
+
+const get_costs_for_crop = (input) => {
+  return get_yield_for_crop(input) * input.crop.cost;
+};
+const get_revenue_for_crop = (input) => {
+  return get_yield_for_crop(input) * input.crop.sales_price;
+};
 
 module.exports = {
+  get_costs_for_crop,
+  get_revenue_for_crop,
+  get_profit_for_crop,
   get_yield_for_plant,
   get_yield_for_crop,
   get_total_yield,
+  get_total_profit,
 };
--- groentetuin.test.js
+++ groentetuin.test.js
@@ -1,17 +1,36 @@
 const {
+  get_costs_for_crop,
+  get_revenue_for_crop,
+  get_profit_for_crop,
   get_yield_for_plant,
   get_yield_for_crop,
   get_total_yield,
+  get_total_profit,
 } = require("./groentetuin");
 
 describe("get_yield_for_plant", () => {
   const corn = {
     name: "corn",
     yield: 30,
+    factors: {
+      sun: {
+        low: -50,
+        medium: 0,
+        high: 50,
+      },
+    },
+  };
+
+  const environment_factors = {
+    sun: "low",
   };
 
   test("Get yield for plant with no environment factors", () => {
     expect(get_yield_for_plant(corn)).toBe(30);
+  });
+
+  test("Get yield for plant with environment factors", () => {
+    expect(get_yield_for_plant(corn, environment_factors)).toBe(15);
   });
 });
 
@@ -53,5 +72,74 @@ describe("get_total_yield", () => {
     };
     const crops = [{ crop: corn, num_crops: 0 }];
     expect(get_total_yield({ crops })).toBe(0);
+  });
+});
+
+describe("get_cost_for_crop", () => {
+  test("Get cost for crop, simple", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+      cost: 0.5,
+    };
+    const input = {
+      crop: corn,
+      num_crops: 10,
+    };
+    expect(get_costs_for_crop(input)).toBe(15);
+  });
+});
+
+describe("get_revenue_for_crop", () => {
+  test("Get revenue for crop, simple", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+      cost: 0.5,
+      sales_price: 2,
+    };
+    const input = {
+      crop: corn,
+      num_crops: 10,
+    };
+    expect(get_revenue_for_crop(input)).toBe(60);
+  });
+});
+
+describe("get_profit_for_crop", () => {
+  test("Get profit for crop, simple", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+      cost: 0.5,
+      sales_price: 2,
+    };
+    const input = {
+      crop: corn,
+      num_crops: 10,
+    };
+    expect(get_profit_for_crop(input)).toBe(45);
+  });
+});
+
+describe("get_total_profit", () => {
+  test("Calculate total profit with multiple crops", () => {
+    const corn = {
+      name: "corn",
+      yield: 3,
+      cost: 0.5,
+      sales_price: 2,
+    };
+    const pumpkin = {
+      name: "pumpkin",
+      yield: 4,
+      cost: 3,
+      sales_price: 4,
+    };
+    const crops = [
+      { crop: corn, num_crops: 10 },
+      { crop: pumpkin, num_crops: 5 },
+    ];
+    expect(get_total_profit({ crops })).toBe(65);
   });
 });
